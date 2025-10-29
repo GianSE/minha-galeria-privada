ffb79255f88b9d561dc20f05bca5f05370045e67
Gian <gianpedrodev@gmail.com>
2025-10-29 00:25:45 -0300

    Fase 3: Reorganizei os arquivos

--- static/js/app.js
+++ static/js/app.js
@@ -2,9 +2,7 @@
 const SUPABASE_URL = 'https://ohxhnegyewnytbvbeuvp.supabase.co'; 
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9oeGhuZWd5ZXdueXRidmJldXZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2OTY2NjIsImV4cCI6MjA3NzI3MjY2Mn0.rqzmNabEI0KyrPx9o604igq_CctSVc81ia_T2KZ_BAA';
 
-// --- CORREÇÃO AQUI ---
-// Era "Supabase.createClient" (errado)
-// Agora é "supabase.createClient" (minúsculo, correto)
+// Correção: "supabase.createClient" (minúsculo)
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // DEBUG: 1. Verifica se o script foi carregado
@@ -38,8 +36,6 @@ document.addEventListener('DOMContentLoaded', () => {
         // DEBUG: 5. Verifica os dados antes de enviar
         console.log('DEBUG: Enviando para o Supabase...', { email: emailInput.value });
         
-        // --- CORREÇÃO AQUI ---
-        // Usando a nova variável "supabaseClient"
         const { data, error } = await supabaseClient.auth.signInWithPassword({
           email: emailInput.value,
           password: passwordInput.value,
@@ -55,12 +51,16 @@ document.addEventListener('DOMContentLoaded', () => {
           messageElement.className = 'message message-success';
 
           setTimeout(() => {
-            window.location.href = 'galeria.html';
+            // --- CORREÇÃO DE CAMINHO ---
+            // Atualiza o redirecionamento para a nova pasta
+            window.location.href = 'templates/galeria.html';
           }, 1500); 
         }
       });
     } else {
-        console.error("DEBUG: FALHA CRÍTICA! Não consegui encontrar o elemento '#login-form' no HTML.");
+        // Se o loginForm for nulo (ex: na galeria.html), isso é esperado.
+        // Podemos adicionar o código da Fase 4/5/6 aqui depois.
+        console.log("DEBUG: Formulário 'login-form' não encontrado. (Isso é normal se não estivermos no index.html)");
     }
 
 });
